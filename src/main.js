7f3ea02c985b15e8c6b6fc18877144eaf61185bf
万松涛 <2294215581@qq.com>
2021-04-02 15:12:57 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(ElementUI).use(store).use(router).mount('#app')
