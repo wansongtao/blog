ec5c6a2447661239b1910975a2897368d3483033
万松涛 <2294215581@qq.com>
2021-04-02 16:24:08 +0800

    样式初始化

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,16 @@
 </template>
 
 <style lang="scss">
+html, body, ul, p, h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'KaiTi', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  font-size: 18px;
 }
 </style>
