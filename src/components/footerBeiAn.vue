cf015c4e701856f07b96f620daf110628c36bde9
万松涛 <2294215581@qq.com>
2021-04-03 10:23:33 +0800

    将备案信息封装到组件中

---
+++ src/components/footerBeiAn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="beian">
+    <a
+      target="_blank"
+      href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44060502002532"
+      style="
+        display: inline-block;
+        text-decoration: none;
+        height: 20px;
+        line-height: 20px;
+      "
+      ><img src="@/assets/images/beian.png" style="float: left" />
+      <p
+        style="
+          float: left;
+          height: 20px;
+          line-height: 20px;
+          margin: 0px 0px 0px 5px;
+          color: #939393;
+        "
+      >
+        粤公网安备 44060502002532号
+      </p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+  .beian {
+    width: 100%;
+    padding: 10px 0;
+    background: #1d1d21;
+    text-align: center;
+    font-size: 16px;
+  }
+</style>
