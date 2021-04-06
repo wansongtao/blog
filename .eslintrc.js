5cea279534ddd3319c9a8f2efba72ceccdb24e1d
万松涛 <2294215581@qq.com>
2021-04-06 17:10:03 +0800

    eslint自定义规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-eval': 'error',
+    'semi': ['error', 'always'], // 必须用;结尾
+    'no-var': 'error',
+    'no-with': 'error',
+    'arrow-parens': ['error', 'always'], // 箭头函数必须用括号
+    'camelcase': 'error' // 强制使用驼峰命名
   }
 }
