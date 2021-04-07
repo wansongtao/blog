8cb65da8ce99d7c034ab6dea8264117d7902fdd9
万松涛 <2294215581@qq.com>
2021-04-07 16:53:04 +0800

    对接接口

--- src/api/article.js
+++ src/api/article.js
@@ -10,3 +10,25 @@ export const getHotArticle = () => {
         method: 'GET'
     });
 };
+
+/**
+ * @description 获取最新文章
+ * @returns {promise}
+ */
+export const getNewArticle = () => {
+    return request({
+        url: '/blog/getnewarticle',
+        method: 'GET'
+    });
+};
+
+/**
+ * @description 搜索文章
+ * @returns {promise}
+ */
+ export const getSearchArticle = (keyword) => {
+    return request({
+        url: `/blog/searcharticle?keyword=${keyword}`,
+        method: 'GET',
+    });
+};
