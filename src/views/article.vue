3dc25404e3e323afc9c75d49a4f029c33ce64412
万松涛 <2294215581@qq.com>
2021-04-07 18:31:33 +0800

    完成文章页布局

--- src/views/article.vue
+++ src/views/article.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>article</div>
+  <div class="container">
+    <h6>最新文章</h6>
+    <div class="article-list" v-if="newList.length > 0">
+      <ArticleCard
+        v-for="(item, index) in newList"
+        :key="index"
+        :article="item"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, reactive, toRefs } from "vue";
+import { getNewArticle } from "@/api/article.js";
+import ArticleCard from "@/components/articleCard.vue";
 
-};
+export default defineComponent({
+  components: {
+    ArticleCard,
+  },
+  setup() {
+    const state = reactive({
+      newList: []
+    });
+
+    getNewArticle().then((data) => {
+      state.newList = data.articles.slice(0, 7);
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  }
+});
 </script>
 
-<style>
+<style lang="scss" scoped>
+.title {
+  height: 40px;
+  font-size: 24px;
+  line-height: 40px;
+  border-bottom: 1px solid #eee;
+  text-align: left;
+}
 
+.container {
+  padding: 10px;
+  background: #fff;
+
+  h6 {
+    @extend .title;
+  }
+
+  .article-list {
+    display: flex;
+    padding: 10px;
+    flex-wrap: wrap;
+    align-content: space-between;
+    justify-content: flex-start;
+  }
+}
 </style>
