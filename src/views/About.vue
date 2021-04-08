86027932b346239fb5098c725e709c6ab77b7f25
万松涛 <2294215581@qq.com>
2021-04-08 16:16:36 +0800

    添加动画样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,19 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.container {
+  animation: main-ani 0.6s linear 0s 1 forwards;
+}
 
+@keyframes main-ani {
+  from {
+    opacity: 0;
+    transform: translateX(-14vw);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
 </style>
